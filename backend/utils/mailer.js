7cd64d33bb1d748b6248d0e829a830f8826f80b8
Mayank Sharma <sharmayank18@gmail.com>
2026-05-28 16:05:26 +0530

    debug smtp production

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,12 +2,17 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
+
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
-  }
+  },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 console.log('📧 Email Config:', {
